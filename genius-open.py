7f878c6e6f50d9166281faafa234ed0daabe8f89
shenchen <shenchen@cogenda.com>
2011-04-05 17:29:55 +0800

    Genius-open source collection.

---
+++ genius-open.py
@@ -0,0 +1,67 @@
+from jiglib.CommonPkgs import *
+
+class Genius(WafPackage):
+    name = 'Genius'
+    version = '20110401'
+    prereqs_src = ['petsc', 'cgnslib', 'vtk']
+    src_url = ['http://download.github.com/cogenda-Genius-TCAD-Open-20110405-0-g7ae2374.tar.gz',
+              '/home/public/software/TCAD/genius-20110401-open.tar.bz2']
+
+    def __init__(self, *args, **kwargs):
+
+        super(Genius, self).__init__(*args, **kwargs)
+
+        petsc = self.deps['petsc']
+        self.waf_args = [
+            '--prefix=${TGTDIR}/genius',
+            '--version=%s' % self.version,
+            '--with-petsc-dir=${TGTDIR}/petsc/%s' % petsc.arch,
+            '--with-petsc-arch=',
+            '--with-cgns-dir=${TGTDIR}',
+            '--with-vtk-dir=${TGTDIR}',
+            #'--with-vtk-ver=vtk-5.4',
+            ]
+
+    def installWorld(self, wldDir, objDir, obj):
+        lst = super(Genius, self).installWorld(wldDir, objDir, obj)
+
+        script='''#!/bin/sh
+export PATH=${TGTDIR}/genius/bin:${TGTDIR}/bin:$PATH
+if [[ "x$LD_LIBRARY_PATH" == "x" ]]
+then
+    export LD_LIBRARY_PATH=${TGTDIR}/lib
+else
+    export LD_LIBRARY_PATH=${TGTDIR}/lib:$LD_LIBRARY_PATH
+fi
+export GENIUS_DIR=${TGTDIR}/genius
+genius.LINUX $*
+'''
+        script = self._subst_vars(script, {'TGTDIR':wldDir})
+
+        writeFile(wldDir, 'genius/bin/genius', script, mode=0755)
+        lst.append(os.path.join(wldDir, 'genius', 'bin', 'genius'))
+
+        return lst
+
+class CogendaTCAD(Collection):
+    _cgnslib = CGNSLib(options=['pic'])
+    _cmake = CMake()
+    _vtk = VTK(_cmake)
+
+    # petsc
+    _petsc_gcc = Petsc(options=['nompi'])
+
+    genius = Genius(_petsc_gcc, _cgnslib, _vtk)
+
+
+if __name__=='__main__':
+    base = BaseSystem()
+
+    repo = Repository(
+            '/usr/local/cogenda/repo',          # Repository dir
+            tmpDir='/home/src/cogenda-open'     # temp dir for building
+                     )
+    tcad = CogendaTCAD(repo, base)
+    tcad.build()                                # Build all packages, add to repo.
+    tcad.install('/home/hash/cogenda')          # Install to this dir
+
